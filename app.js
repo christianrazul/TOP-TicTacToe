a1d107f3ff0fdfd959a72ccd0e5813b069ac3958
christianrazul <razulchristian@gmail.com>
2023-08-18 22:48:28 +0800

    added more styling

---
+++ app.js
@@ -0,0 +1,8 @@
+let BOX = document.querySelector(".box");
+
+BOX.addEventListener("click", () => {
+  alert("HELLO WORLD");
+  const WINNER = document.querySelector(".winner");
+
+  WINNER.style.opacity = "1";
+});
