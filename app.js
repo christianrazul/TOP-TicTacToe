cfea7a9f69458b2f0213844d93d9c05c0adc8cd3
christianrazul <razulchristian@gmail.com>
2023-08-23 14:41:23 +0800

    added basic hard-coded functionality for game loop

--- app.js
+++ app.js
@@ -18,13 +18,32 @@ myArray.forEach((element) => {
 const WINNER = document.querySelector(".winner");
 
 function checkWinner() {
-  if (myArray[2].textContent === "X" && myArray[5].textContent === "X" && myArray[8].textContent === "X") {
+  if (
+    (myArray[0].textContent === "X" && myArray[3].textContent === "X" && myArray[6].textContent === "X") ||
+    (myArray[1].textContent === "X" && myArray[4].textContent === "X" && myArray[7].textContent === "X") ||
+    (myArray[2].textContent === "X" && myArray[5].textContent === "X" && myArray[8].textContent === "X") ||
+    (myArray[0].textContent === "X" && myArray[1].textContent === "X" && myArray[2].textContent === "X") ||
+    (myArray[3].textContent === "X" && myArray[4].textContent === "X" && myArray[5].textContent === "X") ||
+    (myArray[6].textContent === "X" && myArray[7].textContent === "X" && myArray[9].textContent === "X") ||
+    (myArray[0].textContent === "X" && myArray[4].textContent === "X" && myArray[8].textContent === "X") ||
+    (myArray[2].textContent === "X" && myArray[4].textContent === "X" && myArray[6].textContent === "X")
+  ) {
     declareWinner("X");
-  } else if (myArray[2].textContent === "X" && myArray[5].textContent === "X" && myArray[8].textContent === "X") {
-    console.log("X WINS");
+  } else if (
+    (myArray[0].textContent === "O" && myArray[3].textContent === "O" && myArray[6].textContent === "O") ||
+    (myArray[0].textContent === "O" && myArray[3].textContent === "O" && myArray[6].textContent === "O") ||
+    (myArray[1].textContent === "O" && myArray[4].textContent === "O" && myArray[7].textContent === "O") ||
+    (myArray[2].textContent === "O" && myArray[5].textContent === "O" && myArray[8].textContent === "O") ||
+    (myArray[0].textContent === "O" && myArray[1].textContent === "O" && myArray[2].textContent === "O") ||
+    (myArray[3].textContent === "O" && myArray[4].textContent === "O" && myArray[5].textContent === "O") ||
+    (myArray[6].textContent === "O" && myArray[7].textContent === "O" && myArray[9].textContent === "O") ||
+    (myArray[0].textContent === "O" && myArray[4].textContent === "O" && myArray[8].textContent === "O") ||
+    (myArray[2].textContent === "O" && myArray[4].textContent === "O" && myArray[6].textContent === "O")
+  ) {
+    declareWinner("O");
   }
 }
 
 function declareWinner(player) {
-  return (player.textContent = `${player} WINS`), (player.style.opacity = 1);
+  return (WINNER.textContent = `${player} WINS`), (WINNER.style.opacity = 1);
 }
